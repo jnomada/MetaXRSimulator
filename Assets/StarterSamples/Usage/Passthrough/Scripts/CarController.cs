c5bd4e0e8e7e9b24d330d4bfe1e8f93594397804
jnomada <46501701+jnomada@users.noreply.github.com>
2026-04-01 15:50:09 +0200

    assets and scripts

---
+++ Assets/StarterSamples/Usage/Passthrough/Scripts/CarController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using Meta.XR.Samples;
+using System;
+
+// This ensures the GrabObject is always there
+[RequireComponent(typeof(GrabObject))]
+public class CarController : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public ParticleSystem particle;
+    public OVRInput.Controller controllerHand = OVRInput.Controller.None;
+
+    void Start()
+    {
+        if (!audioSource) audioSource = GetComponent<AudioSource>();
+        
+        // GrabObject is the component from Meta Samples that handles the physics/trigger
+        GrabObject grabScript = GetComponent<GrabObject>();
+        if (grabScript != null)
+        {
+            grabScript.GrabbedObjectDelegate += Grab;
+            grabScript.ReleasedObjectDelegate += Release;
+        }
+    }
+
+    public void Grab(OVRInput.Controller grabHand)
+    {
+        Debug.Log("Car Grabbed by: " + grabHand);
+        controllerHand = grabHand;
+        if (audioSource) audioSource.Play();
+        if (particle) particle.Play();
+    }
+
+    public void Release()
+    {
+        Debug.Log("Car Released");
+        controllerHand = OVRInput.Controller.None;
+        if (audioSource) audioSource.Stop();
+        if (particle) particle.Stop();
+    }
+}
